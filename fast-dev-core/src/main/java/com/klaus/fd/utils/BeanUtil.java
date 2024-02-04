427651ea3bf27b35d787e35402d2ec62f7e8d186
Silas <klaus.cicd@outlook.com>
2024-02-04 14:17:49 +0800

    optimize: BeanUtil方法名称优化

--- fast-dev-core/src/main/java/com/klaus/fd/utils/BeanUtil.java
+++ fast-dev-core/src/main/java/com/klaus/fd/utils/BeanUtil.java
@@ -6,6 +6,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.lang.Nullable;
 
 import java.util.Map;
 
@@ -19,7 +20,7 @@ import java.util.Map;
 public class BeanUtil extends BeanUtils implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
 
-    public <T> T getBeanByType(Class<T> targetClass) {
+    public static <T> T getBean(Class<T> targetClass) {
         return applicationContext.getBean(targetClass);
     }
 
@@ -32,16 +33,30 @@ public class BeanUtil extends BeanUtils implements ApplicationContextAware {
      * @param clazz clazz
      * @return {@link Map}<{@link String}, {@link T}>
      */
-    public static <T> Map<String, T> getBeansOfType(Class<T> clazz) {
+    public static <T> Map<String, T> getBeans(Class<T> clazz) {
+        checkApplicationContext();
         return applicationContext.getBeansOfType(clazz);
     }
 
-    public static Object getBean(String name) {
-        return applicationContext.getBean(name);
+    /**
+     * 从静态变量ApplicationContext中取得Bean, 自动转型为所赋值对象的类型.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) {
+        checkApplicationContext();
+        return (T) applicationContext.getBean(name);
     }
 
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        BeanUtil.applicationContext = applicationContext;
+    public void setApplicationContext(@Nullable ApplicationContext context) throws BeansException {
+        if (applicationContext == null) {
+            applicationContext = context;
+        }
+    }
+
+    private static void checkApplicationContext() {
+        if (applicationContext == null) {
+            throw new IllegalStateException("BeanUtil.applicationContext未注入");
+        }
     }
 }
